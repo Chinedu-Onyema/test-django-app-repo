84596aec07ef155ea9d6ecf9bcd209b8e87bb2e7
Chinedu Onyema <Onyemachinedu45@gmail.com>
2026-09-26 21:00:37 +0000

    change

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git pull origin main
 
 DOCKERHUB_USERNAME="edunaking"           # replace with your dockerhub username 
                                                        # for example edunaking
